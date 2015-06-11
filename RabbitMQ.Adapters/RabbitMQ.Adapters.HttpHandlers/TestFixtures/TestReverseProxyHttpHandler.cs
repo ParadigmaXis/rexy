e17e4cf3547d6d8362228bdd1992b54ace7dde29
Filipe Correia <filipe.correia@paradigmaxis.pt>
2015-06-11 14:17:40 +0100

    Moved code from the "RunMe" test to a setup method and to a more-appropriately-named "CreateBasicProperties" test.

--- RabbitMQ.Adapters/RabbitMQ.Adapters.HttpHandlers/TestFixtures/TestReverseProxyHttpHandler.cs
+++ RabbitMQ.Adapters/RabbitMQ.Adapters.HttpHandlers/TestFixtures/TestReverseProxyHttpHandler.cs
@@ -12,18 +12,27 @@ using System.Web;
 namespace RabbitMQ.Adapters.HttpHandlers.TestFixtures {
     [TestFixture]
     public class TestReverseProxyHttpHandler: AssertionHelper {
-        [Test]
-        public void RunMe() {
-            var requestMethod = "POST";
-            var requestGatewayUrl = new Uri("http://localhost:8888/adapter/helloworld/HelloWorld.asmx");
-            var requestDestinationUrl = new Uri("http://localhost:8888/helloworld/HelloWorld.asmx");
-            var requestHeaders = new Dictionary<String, String>() {
+        private string requestMethod;
+        private Uri requestGatewayUrl;
+        private Uri requestDestinationUrl;
+        private Dictionary<String, String> requestHeaders;
+        private string requestBody;
+        private bool requestIsAuthenticated;
+        private WindowsIdentity requestLogonUserIdentity;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.requestMethod = "POST";
+            this.requestGatewayUrl = new Uri("http://localhost:8888/adapter/helloworld/HelloWorld.asmx");
+            this.requestDestinationUrl = new Uri("http://localhost:8888/helloworld/HelloWorld.asmx");
+            this.requestHeaders = new Dictionary<String, String>() {
                 { "Host", "localhost" },
                 { "Content-Type", "text/xml; charset=utf-8" },
                 { "Content-Length", "0" },
                 { "SOAPAction", "http://www.paradigmaxis.pt/isa/2015/06/08/hello-world/HelloWorld" }
             };
-            var requestBody =
+            this.requestBody =
                 "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
                 "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
                 "  <soap:Body>" +
@@ -32,21 +41,31 @@ namespace RabbitMQ.Adapters.HttpHandlers.TestFixtures {
                 "    </HelloWorld>" +
                 "  </soap:Body>" +
                 "</soap:Envelope>";
-            var requestIsAuthenticated= false;
-            var requestLogonUserIdentity = (WindowsIdentity)null;
+            this.requestIsAuthenticated = false;
+            this.requestLogonUserIdentity = (WindowsIdentity)null;
+        }
 
-            var basicProperties = new ReverseProxyHttpHandler().CreateBasicProperties(requestMethod, requestGatewayUrl, requestDestinationUrl, requestHeaders, requestIsAuthenticated);
+        [Test]
+        public void CreateBasicProperties()
+        {
+            var basicProperties = new ReverseProxyHttpHandler().CreateBasicProperties(this.requestMethod, this.requestGatewayUrl, this.requestDestinationUrl, this.requestHeaders, this.requestIsAuthenticated);
             Expect(basicProperties, Is.Not.Null);
             Expect(basicProperties.Headers, Is.Not.Null);
-            Expect(basicProperties.Headers.Count, Is.EqualTo(4 + requestHeaders.Count));
-            Expect(basicProperties.Headers.Keys, Is.EquivalentTo(requestHeaders.Keys.Select(k => "http-" + k).Concat(new String[] { Constants.RequestMethod, Constants.RequestGatewayUrl, Constants.RequestDestinationUrl, Constants.RequestIsAuthenticated })));
-            
+            Expect(basicProperties.Headers.Count, Is.EqualTo(4 + this.requestHeaders.Count));
+            Expect(basicProperties.Headers.Keys, Is.EquivalentTo(this.requestHeaders.Keys.Select(k => "http-" + k).Concat(new String[] { Constants.RequestMethod, Constants.RequestGatewayUrl, Constants.RequestDestinationUrl, Constants.RequestIsAuthenticated })));
+        }
+
+        [Test]
+        public void RunMe()
+        {
             // Operation
 
             var responseStatusCode = Int32.MinValue;
             var responseStatusDescription = String.Empty;
             var responseHeaders = new Dictionary<String, String>();
             var responseBody = new byte[0];
+            Assert.IsTrue(false);
         }
+
     }
 }
