a60f9bcbfc4a6f737fd02e2f4c8ec5caab971032
José Vilaça <jose.vilaca@paradigmaxis.pt>
2015-06-11 12:20:16 +0100

    Added NUnit test support.

---
+++ RabbitMQ.Adapters/RabbitMQ.Adapters.HttpHandlers/TestFixtures/TestReverseProxyHttpHandler.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Adapters.HttpHandlers.TestFixtures {
+    [TestFixture]
+    public class TestReverseProxyHttpHandler: AssertionHelper {
+        [Test]
+        public void RunMe() {
+            Assert.Fail();
+        }
+    }
+}
---
+++ RabbitMQ.Adapters/RabbitMQ.Adapters.WebServiceCaller/TestFixtures/TestWebServiceCaller.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Adapters.WebServiceCaller {
+    [TestFixture]
+    public class TestWebServiceCaller {
+        [Test]
+        public void RunMe() {
+            Assert.Fail();
+        }
+    }
+}
