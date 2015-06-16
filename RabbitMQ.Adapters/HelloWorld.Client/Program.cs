2c9ec1e1dcbe0496e8ee96dfe740270849f8d2ed
José Vilaça <jose.vilaca@paradigmaxis.pt>
2015-06-16 18:00:27 +0100

    Added HelloWorld.Client, a console program that calls the web service implemented in HelloWorld.Services.

---
+++ RabbitMQ.Adapters/HelloWorld.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Client {
+    class Program {
+        static void Main(string[] args) {
+            var start = DateTime.UtcNow;
+            var tasks = new List<Task>();
+            for (int i = 0; i < 20; i++) {
+                tasks.Add(Task.Factory.StartNew(RunIt));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("All DONE {0}", DateTime.UtcNow - start);
+            Console.ReadKey();
+        }
+        static void RunIt() {
+            var start = DateTime.UtcNow;
+            var client = new ServiceReferences.HelloWorldServiceSoapClient("HelloWorldServiceSoap");
+            client.ClientCredentials.UseIdentityConfiguration = true;
+            client.ClientCredentials.Windows.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+            client.ClientCredentials.Windows.ClientCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+            var txt = client.HelloWorld("Hello??");
+            Console.WriteLine("DONE {0}: {1}", txt, DateTime.UtcNow - start);
+        }
+    }
+}
