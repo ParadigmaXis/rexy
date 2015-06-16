034c1b916dc576d19ac4a7a79e96b198e8b3035d
José Vilaça <jose.vilaca@paradigmaxis.pt>
2015-06-16 17:58:39 +0100

    Force the loading of the Microsoft.Samples.Security.SSPI assembly on application start, otherwise it may attempt to load while impersonating a request user.

--- RabbitMQ.Adapters/RabbitMQ.Adapters.HttpHandlers/Global.asax.cs
+++ RabbitMQ.Adapters/RabbitMQ.Adapters.HttpHandlers/Global.asax.cs
@@ -9,8 +9,12 @@ using System.Web.SessionState;
 
 namespace RabbitMQ.Adapters.HttpHandlers {
     public class Global : System.Web.HttpApplication {
+        
 
         protected void Application_Start(object sender, EventArgs e) {
+            using (var thisVariableForcesTheLoadingOfTheAssembly = new Microsoft.Samples.Security.SSPI.ClientContext(new Microsoft.Samples.Security.SSPI.ClientCredential(Microsoft.Samples.Security.SSPI.Credential.Package.Negotiate), "", Microsoft.Samples.Security.SSPI.ClientContext.ContextAttributeFlags.Delegate)) {
+            }
+
             var factory = new ConnectionFactory { HostName = "AURA", VirtualHost = "/", UserName = "isa-http-handler", Password = "isa-http-handler" };
             using (var connection = factory.CreateConnection()) {
                 using (var channel = connection.CreateModel()) {
