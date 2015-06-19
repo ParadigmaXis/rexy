19610f603a2eea6961776b9b96c0ed2d20cfccce
Joao Ferreira <joao.ferreira@paradigmaxis.pt>
2015-06-19 16:05:44 +0100

    Log when a route destination URL is invalid

--- RabbitMQ.Adapters/RabbitMQ.Adapters.Route/ParseRoutesFile.cs
+++ RabbitMQ.Adapters/RabbitMQ.Adapters.Route/ParseRoutesFile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,9 @@ using System.Xml.Linq;
 
 namespace RabbitMQ.Adapters.Routes {
     public class ParseRoutesFile {
+
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public ParseRoutesFile() { }
 
         internal XElement LoadRawRoutes(string filePath) {
@@ -19,6 +23,11 @@ namespace RabbitMQ.Adapters.Routes {
             rawRoutes.Descendants("route").ToList().ForEach(r => {
                 if (ValidateDestinationUrl(r.Element("destination").Value, out uri))
                     routes.Add(new Route(r.Attribute("name").Value, r.Element("path").Value, uri));
+                else {
+                    logger.ErrorFormat("Invalid Uri: {0}" + Environment.NewLine + 
+                        "\tConnection name: {1}" + Environment.NewLine + 
+                        "\tPath: {2}", r.Element("destination").Value, r.Attribute("name").Value, r.Element("path").Value);
+                }   
             });
             return routes;
         }
