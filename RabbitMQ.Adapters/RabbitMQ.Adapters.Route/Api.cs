113c4a6b6ebf37ac857845e677805c03153b2f90
Joao Ferreira <joao.ferreira@paradigmaxis.pt>
2015-06-19 12:34:21 +0100

    Added project for configuring the routing call to WebServices using a XML file

---
+++ RabbitMQ.Adapters/RabbitMQ.Adapters.Route/Api.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Adapters.Route;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Adapters.Route {
+    public class Api {
+        private static string ROUTES_FILE_PATH = AppDomain.CurrentDomain.BaseDirectory + "\\routes.xml";
+        private IEnumerable<Route> routes;
+
+        private static Api _api;
+        public static Api GetApi {
+            get {
+                if (_api == null)
+                    _api = new Api();
+                return _api;
+            }
+        }
+
+        private Api() {
+            var parser = new ParseRoutesFile();
+            routes = parser.XmlToRoutes(parser.LoadRawRoutes(ROUTES_FILE_PATH));
+        }
+
+        public Route GetRoute(string origin) {
+            return routes.SingleOrDefault(r => r.Path.Equals(origin));
+        }
+    }
+}
